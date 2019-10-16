8e7e3c5422413400e0bae9fb4092783eb23ccd6b
Tareq Karimi <m.tareq.karimi@gmail.com>
2019-10-16 11:22:11 -0400

    Added a sample ReactNative app to start with

---
+++ StepUp-ReactNative/screens/LinksScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+// import { TestView } from '../screens';
+
+export default function LinksScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      {/**
+       * Go ahead and delete ExpoLinksView and replace it with your content;
+       * we just wanted to provide you with some helpful links.
+       */}
+      <ExpoLinksView />
+      {/* <TestView /> */}
+    </ScrollView>
+  );
+}
+
+LinksScreen.navigationOptions = {
+  title: 'Links',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
