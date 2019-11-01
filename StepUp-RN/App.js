2240be9f4686398c38a566ae7d61096b20b6ac4d
kelvintrinh174 <kelvintrinh174@gmail.com>
2019-11-01 11:47:05 -0400

    jj

--- StepUp-RN/App.js
+++ StepUp-RN/App.js
@@ -1,44 +1,17 @@
 import React, { Component } from 'react';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { Root } from "native-base";
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
-import HomeScreen from './screens/HomeScreen';
-import Profile from './screens/ProfileScreen';
-
-const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  Profile: {screen: Profile},
-});
-
-
-const AppContainer = createAppContainer(MainNavigator);
+import Profile from './screens/Profile/Profile';
 
 
 export default class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      isReady: false,
-    };
-  }
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
-    });
-    this.setState({ isReady: true });
-  }
-
   render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
-
-    return <AppContainer />
+    return     <Root>
+           <Profile />
+  </Root>;
   }
 }
- 
+
+
