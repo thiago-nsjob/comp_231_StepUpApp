7e43162e024adac657c7c8666bc1f487743cec04
kelvintrinh174 <kelvintrinh174@gmail.com>
2019-11-04 21:02:35 -0500

    fff

--- StepUp-RN/App.js
+++ StepUp-RN/App.js
@@ -5,28 +5,17 @@ import { AppLoading } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import Profile from './screens/Profile/Profile';
 
-<<<<<<< HEAD
-
-export default class App extends React.Component{
-  render() {
-    return     <Root>
-           <Profile />
-=======
 import{wrapIntoContext} from './components/Wrapper';
 
 class App extends React.Component{
   render() {
     return <Root>
            <Profile {...this.props} />
->>>>>>> 2fbdf9f309f0af59c9be3396ad00fa48b8b0f083
   </Root>;
   }
 }
 
-<<<<<<< HEAD
-=======
 export default wrapIntoContext(App);
 
 
->>>>>>> 2fbdf9f309f0af59c9be3396ad00fa48b8b0f083
 
